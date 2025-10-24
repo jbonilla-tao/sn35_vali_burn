11ba23a86b7a51be7252c27fe9370c37fc50a28e
Tony <tony.leviethung@gmail.com>
2025-10-24 14:04:55 -0700

    update miner script

--- neuron/__init__.py
+++ neuron/__init__.py
@@ -1,1 +1,10 @@
+<<<<<<< Updated upstream
 # Neuron package for liquidity flow controller
+=======
+"""
+Neuron package exposes miner and validator entrypoints for liquidity flow control.
+"""
+
+__all__ = ["miner", "validator"]
+
+>>>>>>> Stashed changes
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,8 @@
+<<<<<<< Updated upstream
 # Utils package for liquidity flow controller
+=======
+"""
+Utility helpers shared across the liquidity flow controller scripts.
+"""
+
+>>>>>>> Stashed changes
